41c0e76122365c6abd571c2f2590b6b79e245e1b
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-07-30 14:37:33 -0300

    validando-dados-com-yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,23 @@
 import { v4 } from 'uuid';
 import User from '../models/User';
+import * as Yup from 'yup'
+
 
 class UserController {
     async store(request, response) {
+        const schema  = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        });
+
+        try {
+            await schema.validate(request.body, {abortEarly: false});
+        } catch (error) {
+            return response.status(400).json({ error: error.errors });
+        }
+
         const { name, email, password_hash, admin } = request.body;
         const user = await User.create({
             id: v4(),
@@ -11,6 +26,7 @@ class UserController {
             password_hash,
             admin,
         });
+
         return response.status(200).json({
             id: user.id, 
             name,
