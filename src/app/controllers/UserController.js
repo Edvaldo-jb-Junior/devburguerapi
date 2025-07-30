cfb747dbdf6ec250cba14ef5f0aaa4731ed9c483
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-07-30 14:57:23 -0300

    criptografia-senha-bcrypt

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,7 +8,7 @@ class UserController {
         const schema  = Yup.object({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
-            password_hash: Yup.string().min(6).required(),
+            password: Yup.string().min(6).required(),
             admin: Yup.boolean(),
         });
 
@@ -18,12 +18,21 @@ class UserController {
             return response.status(400).json({ error: error.errors });
         }
 
-        const { name, email, password_hash, admin } = request.body;
+        const { name, email, password, admin } = request.body;
+
+        const userExists = await User.findOne({ where: {
+            email,
+        },
+    });
+
+        if (userExists) {
+            return response.status(400).json({ error: 'User already exists' });
+        }
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin,
         });
 
